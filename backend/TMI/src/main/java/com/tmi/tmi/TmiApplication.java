cb3436198eb4c463c8c11dc88dda1517a1472826
최낙훈 <cnh6123@naver.com>
2021-04-27 17:51:16 +0900

    [Fix] Modify TMI Data Project Build Error Fix
    S04P31A201-175

--- backend/TMI/src/main/java/com/tmi/tmi/TmiApplication.java
+++ backend/TMI/src/main/java/com/tmi/tmi/TmiApplication.java
@@ -2,6 +2,7 @@ package com.tmi.tmi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,7 +11,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableAutoConfiguration(exclude={MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 @ComponentScan
-//@SpringBootApplication
+@SpringBootApplication
 public class TmiApplication {
     public static void main(String[] args) {
         SpringApplication.run(TmiApplication.class, args);
