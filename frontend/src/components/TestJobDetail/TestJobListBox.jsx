c9da33bac244a2be797537fe205e8bc8f1800b7f
이병희 <milk1543@naver.com>
2021-04-27 18:42:17 +0900

    [Feat] Add TestJobItemContainer Component
    S04P31A201-222

--- frontend/src/components/TestJobDetail/TestJobListBox.jsx
+++ frontend/src/components/TestJobDetail/TestJobListBox.jsx
@@ -2,3 +2,5 @@ import React from "react";
 import { Box, Container, Grid, Pagination } from "@material-ui/core";
 
 export default function TestJobListBox() {}
+
+// 직은 페이지만 만들면됌
--- frontend/src/pages/testjob/TestJobDetail.jsx
+++ frontend/src/pages/testjob/TestJobDetail.jsx
@@ -1,12 +1,85 @@
 import React from "react";
 import { useParams } from "react-router";
+import TestJobItemContainer from "../../components/TestJobDetail/TestJobItemContainer";
 
 export default function TestJobDetail() {
-  const path = useParams();
+  // const path = useParams();
+
+  // axios.get() Project의 TestJobList 반환받는다
+
   return (
     <div>
-      TestJobDetail
-      {path.id} test
+      <div
+        style={{
+          marginTop: "1%",
+          marginBottom: "1%",
+        }}
+      >
+        <TestJobItemContainer
+          name="DashBoardDataComponent should run #getCoverageTotal()"
+          time="143"
+          color="10px solid #00FF00"
+          secondColor="8px solid #00DD00"
+          resultType="Success"
+          errorType="none of content"
+          errorMessage="none of content"
+          errorContents="none of content"
+        />
+      </div>
+
+      <div
+        style={{
+          marginTop: "1%",
+          marginBottom: "1%",
+        }}
+      >
+        <TestJobItemContainer
+          name="DashBoardDataComponent should run #getCoverageTotal()"
+          time="143"
+          color="10px solid #FFAA00"
+          secondColor="8px solid #FFCC66"
+          resultType="Fail"
+          errorType="none of content"
+          errorMessage="none of content"
+          errorContents="none of content"
+        />
+      </div>
+
+      <div
+        style={{
+          marginTop: "1%",
+          marginBottom: "1%",
+        }}
+      >
+        <TestJobItemContainer
+          name="DashBoardDataComponent should run #getCoverageTotal()"
+          time="143"
+          color="10px solid #FF0000"
+          secondColor="8px solid #FF4444"
+          resultType="Error"
+          errorType="none of content"
+          errorMessage="none of content"
+          errorContents="none of content"
+        />
+      </div>
+
+      <div
+        style={{
+          marginTop: "1%",
+          marginBottom: "1%",
+        }}
+      >
+        <TestJobItemContainer
+          name="DashBoardDataComponent should run #getCoverageTotal()"
+          time="143"
+          color="10px solid #AAAAAA"
+          secondColor="8px solid #CCCCCC"
+          resultType="Skip"
+          errorType="none of content"
+          errorMessage="none of content"
+          errorContents="none of content"
+        />
+      </div>
     </div>
   );
 }
