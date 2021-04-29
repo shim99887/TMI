e090938d168c3c0e18b462f52a967154eafb0d02
최낙훈 <cnh6123@naver.com>
2021-04-29 17:55:40 +0900

    [Init] Add TestData Model
    S04P31A201-181

--- backend/TmiProject/src/main/java/com/tmi/entity/Project.java
+++ backend/TmiProject/src/main/java/com/tmi/entity/Project.java
@@ -1,13 +1,13 @@
 package com.tmi.entity;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Getter
@@ -21,5 +21,11 @@ public class Project {
     private String description;
     private String regDate;
     private String user;
+    
+    private String javaVersion; //java.version
+    private String branch; //git.branch
+    private String commitId; //git.commit.id (e2fbc561309d03d92a0958f3cf59219b1fc0d985)
+    private String commitUserName; //git.commit.user.name
+    //private String commitMessage; //git.commit.message.short
 
 }
