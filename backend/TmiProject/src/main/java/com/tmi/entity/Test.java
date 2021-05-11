af7a5a1097b4433703b61290e1da91d200a17c41
백현오 <sss9024@naver.com>
2021-05-11 15:58:00 +0900

    [Feat] Add sorted report list
    S04P31A201-329

--- backend/TmiProject/src/main/java/com/tmi/entity/Test.java
+++ backend/TmiProject/src/main/java/com/tmi/entity/Test.java
@@ -2,25 +2,28 @@ package com.tmi.entity;
 
 import javax.persistence.*;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.*;
 
 import java.io.Serializable;
 
 @Entity
 @Data
-@Table(name="test")
+@Table(name = "test")
 public class Test {
 
-    //PK
+    // PK
     @Id
     @GeneratedValue
     private Long id;
 
     // FK
     @ManyToOne
+    @JsonIgnore
     private Report report;
 
-    private String name; //ex) org.springframework.samples.petclinic.owner.VisitControllerTests
+    private String name; // ex) org.springframework.samples.petclinic.owner.VisitControllerTests
     private String type;
     private String errorType;
     private String errorMessage;
@@ -29,7 +32,7 @@ public class Test {
     public Test() {
     }
 
-    public Test(Test newTest){
+    public Test(Test newTest) {
         this.id = newTest.getId();
         this.name = newTest.getName();
         this.type = newTest.getType();
@@ -38,6 +41,5 @@ public class Test {
         this.elapsedTime = newTest.getElapsedTime();
     }
 
-    //txt에 대한 property만 가져옴. xml 파일의 정보도 추가 고려해야함.
+    // txt에 대한 property만 가져옴. xml 파일의 정보도 추가 고려해야함.
 }
-
