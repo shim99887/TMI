f026466f821b80256224b3a4ba9fd607782e6146
powerticket <jwp0530@gmail.com>
2021-05-11 09:37:45 +0900

    [Feat] Add History collapse

--- frontend/src/components/project/ProjectReportCoverageList.jsx
+++ frontend/src/components/project/ProjectReportCoverageList.jsx
@@ -26,7 +26,7 @@ function createData(name, calories, fat, carbs, protein) {
 const rows = [];
 
 const tableHead = [
-  "ID",
+  "App Name",
   "Build Datetime",
   "Line Cov.(%)",
   "Branch Cov.(%)",
@@ -103,17 +103,40 @@ function Row({ row }) {
         <TableCell>{row.totalElapsedTime}</TableCell>
       </TableRow>
       {/* Collapse */}
-      <Collapse in={open} timeout="auto" unmountOnExit>
-        <TableRow>
-          <TableCell>Hi</TableCell>
-          <TableCell>Hi</TableCell>
-          <TableCell>Hi</TableCell>
-          <TableCell>Hi</TableCell>
-          <TableCell>Hi</TableCell>
-          <TableCell>Hi</TableCell>
-          <TableCell>Hi</TableCell>
-        </TableRow>
-      </Collapse>
+      <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={7}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box margin={1}>
+              <Typography variant="h6" gutterBottom component="div">
+                History
+              </Typography>
+              <Table size="small" aria-label="purchases">
+                <TableHead>
+                  <TableRow>
+                    {tableHead.slice(1).map((head, index) => (
+                      <TableCell key={index}>{head}</TableCell>
+                    ))}
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {/* {row.history.map((historyRow) => (
+                    <TableRow key={historyRow.date}>
+                      <TableCell component="th" scope="row">
+                        {historyRow.date}
+                      </TableCell>
+                      <TableCell>{historyRow.customerId}</TableCell>
+                      <TableCell align="right">{historyRow.amount}</TableCell>
+                      <TableCell align="right">
+                        {Math.round(historyRow.amount * row.price * 100) / 100}
+                      </TableCell>
+                    </TableRow>
+                  ))} */}
+                </TableBody>
+              </Table>
+            </Box>
+          </Collapse>
+        </TableCell>
+      </TableRow>
     </>
   );
 }
