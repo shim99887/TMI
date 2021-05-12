5ac39d381e88280a89becc0d83add8ab86d46908
백현오 <sss9024@naver.com>
2021-05-12 09:53:29 +0900

    [Feat] Add login page
    S04P31A201-331

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import Login from "./pages/login/Login";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
 
