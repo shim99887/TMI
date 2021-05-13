1635e5c6b3cea138867c9af6a37b697567b3ac25
최낙훈 <cnh6123@naver.com>
2021-05-13 10:56:16 +0900

    [Feat] Modify App table id standard & data save complete
    S04P31A201-319

--- backend/TmiProject/src/main/java/com/tmi/encrypt/EncryptHandler.java
+++
@@ -1,7 +0,0 @@
-package com.tmi.encrypt;
-
-public interface EncryptHandler {
-	
-	String encrypt(String password);
-	boolean isMatch(String password, String hashed);
-}
--- backend/TmiProject/src/main/java/com/tmi/repository/AppRepository.java
+++ backend/TmiProject/src/main/java/com/tmi/repository/AppRepository.java
@@ -2,9 +2,14 @@ package com.tmi.repository;
 
 import java.util.List;
 
-import com.tmi.entity.App;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.tmi.entity.App;
 
 public interface AppRepository extends JpaRepository<App, String> {
     List<App> findAllByProjectIdEquals(Long pid);
+    
+    @Query("select m from App m where title = :title and git_url = :gitUrl")
+    App findApp(String title, String gitUrl);
 }
